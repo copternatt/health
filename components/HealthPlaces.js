10c277d39b5562a9d69c755cc237e9ef9af5d291
Natthaphol <copter1508@gmail.com>
2025-04-17 23:59:50 +0700

    first commit

---
+++ components/HealthPlaces.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
+import PlaceCard from "./PlaceCard_health";
+import { PLACES } from '../data/places_health';
+
+
+
+const WorkPlaces = ({ navigation }) => {
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>การงาน</Text>
+      {PLACES.map((place) => (
+        <PlaceCard key={place.id} data={place} navigation={navigation} />
+      ))}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#FFF" },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginLeft: 20,
+    textAlign: "center",
+  },
+});
+
+export default WorkPlaces;
